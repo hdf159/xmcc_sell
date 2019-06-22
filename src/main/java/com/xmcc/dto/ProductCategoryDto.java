21f00c88a999b5845e4764da0f32b3f8bb24d7bb
hdf159 <1440988597@qq.com>
2019-06-22 16:43:48 +0800

    2019-6-22 编写了商品列表接口

---
+++ src/main/java/com/xmcc/dto/ProductCategoryDto.java
@@ -0,0 +1,31 @@
+package com.xmcc.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.xmcc.entity.ProductCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serializable;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCategoryDto implements Serializable {
+    @JsonProperty("name")
+    private String categoryName;
+    @JsonProperty("type")
+    private Integer categoryType;
+    @JsonProperty("foods")
+    private List<ProductInfoDto> productInfoDtoList;
+
+   public static ProductCategoryDto build(ProductCategory productCategory){
+       ProductCategoryDto productCategoryDto = new ProductCategoryDto();
+       BeanUtils.copyProperties(productCategory,productCategoryDto);
+       return productCategoryDto;
+
+   }
+
+
+}
